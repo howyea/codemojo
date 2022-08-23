09a003bcfd975019b2ae48aa8f9907d8771a002e
root <root@LAPTOP-8NIBPHF3.localdomain>
2022-08-23 22:14:02 +0800

    add exam style

--- exam-react-ts/src/App.tsx
+++ exam-react-ts/src/App.tsx
@@ -5,7 +5,7 @@ import './App.scss'
 function App() {
 
   return (
-    <>
+    <div className="app">
       <div className="questionNum"><span className="current">1</span>/<span className="all">10</span></div>
       <div className="question">这里是一个问题？</div>
       <div>
@@ -13,12 +13,12 @@ function App() {
         <div className="answerItem"><div className="singleChoice"></div><div className="word">不清楚</div></div>
         <div className="answerItem"><div className="singleChoice"></div><div className="word">不知道</div></div>
       </div>
-      <div>这里是解读</div>
-      <div>
-        <div>上一题</div>
-        <div>下一题</div>
+      <div className="explain">这里是解读</div>
+      <div className="btnBox">
+        <div className="btn">上一题</div>
+        <div className="btn">下一题</div>
       </div>
-    </>  
+    </div>  
   )
 }
 
