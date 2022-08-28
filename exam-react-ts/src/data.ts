89333cb0a60e20191ef0fe86a0642aef9680e919
howyea <yhynew@126.com>
2022-08-28 16:58:47 +0800

    add data file

---
+++ exam-react-ts/src/data.ts
@@ -0,0 +1,36 @@
+interface QuestionType {
+  question: string;
+  answer: string[];
+  explain: string;
+}
+const questions: QuestionType[] = [
+  {
+    "question": "这个是问题一？",
+    "answer": ["不知道", "不清楚", "清楚"],
+    "explain": "这里是个说明。"
+  },
+  {
+    "question": "这个是问题二？",
+    "answer": ["不知道", "不清楚", "清楚"],
+    "explain": "这里是个说明。"
+  },
+  {
+    "question": "这个是问题三？",
+    "answer": ["不知道", "不清楚", "清楚"],
+    "explain": "这里是个说明。"
+  },
+  {
+
+    "question": "这个是问题四？",
+    "answer": ["不知道", "不清楚", "清楚"],
+    "explain": "这里是个说明。"
+  },
+  {
+    "question": "这个是问题五？",
+    "answer": ["不知道", "不清楚", "清楚"],
+    "explain": "这里是个说明。"
+  },
+];
+
+export default questions;
+
