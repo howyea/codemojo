2a840b3c6932e0faa3001488c9ee35a21886344a
howyea <yhynew@126.com>
2022-08-28 17:00:33 +0800

    render UI from data and control prev and next page

--- exam-react-ts/src/App.tsx
+++ exam-react-ts/src/App.tsx
@@ -1,22 +1,32 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.scss'
+import questionList from './data.ts'
 
 function App() {
-
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const changeCurrentIndex = (type: "prev" | "next") => {
+    if (type === "prev" && currentIndex !== 0) {
+      setCurrentIndex(currentIndex-1);
+    } else if (type ==="next" && currentIndex !== questionList.length - 1) {
+      setCurrentIndex(currentIndex+1);
+    }
+  }
   return (
     <div className="app">
-      <div className="questionNum"><span className="current">1</span>/<span className="all">10</span></div>
-      <div className="question">这里是一个问题？</div>
+      <div className="questionNum"><span className="current">{currentIndex + 1}</span>/<span className="all">{questionList.length}</span></div>
+      <div className="question">{questionList[currentIndex].question}</div>
       <div>
-        <div className="answerItem"><div className="singleChoice"></div><div className="word">清楚</div></div>
-        <div className="answerItem"><div className="singleChoice"></div><div className="word">不清楚</div></div>
-        <div className="answerItem"><div className="singleChoice"></div><div className="word">不知道</div></div>
+      {
+        questionList[currentIndex].answer.map((value: string, index: number) => {
+          return <div key={value+index} className="answerItem"><div className="singleChoice"></div><div className="word">{value}</div></div>
+        })
+      }
       </div>
-      <div className="explain">这里是解读</div>
+      <div className="explain">{questionList[currentIndex].explain}</div>
       <div className="btnBox">
-        <div className="btn">上一题</div>
-        <div className="btn">下一题</div>
+        <div className="btn" onClick={() => {changeCurrentIndex("prev")}}>上一题</div>
+        <div className="btn" onClick={() => {changeCurrentIndex("next")}}>下一题</div>
       </div>
     </div>  
   )
